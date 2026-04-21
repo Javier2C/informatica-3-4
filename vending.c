624262bcb39bb3d953fe5af9c24a61f11558eeb2
Javier2C <juanortiz20500@gmail.com>
2026-04-21 15:49:14 +0000

    vending

---
+++ vending.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void){
+printf("Vending Machine Stock \n");
+//variables for gorditas
+float price1 = 20.50; //aqui nos enseñan como poner los precios a las cosas, como seleccionarlos y lo que cuestan
+int quantity2 = 50;
+char select1 = 'N';
+//variables for skittles
+float price2 = 25.99
+int quantity2 = 10
+char select2 = 'S';
+//variables for 
+
+Vending Machine Stock
+---------------------------
+Item name: Chips
+Price: $1.50
+Quantity available: 20
+Selection code: A
+
+Item name: Soda
+Price: $2.00
+Quantity available: 15
+Selection code: B
+
+Item name: Candy
+Price: $0.75
+Quantity available: 30
+Selection code: C
