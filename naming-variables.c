2041ad5ac8f6d6f8cd123a45740415588442c7a0
Javier2C <juanortiz20500@gmail.com>
2026-04-21 14:57:43 +0000

    naming-variables

---
+++ naming-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//i added include
+int main(void)
+{
+int number;
+//i deleted the !
+int test_value;
+//i added a _ in the blank space
+int example;
+//i deleted the number 1
+float int_count ;
+//i added _count
+    printf("Variables created successfully.\n");
+}
