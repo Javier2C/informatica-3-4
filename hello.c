ab693ef773df39fb2f1a90d599c4c2f1b52cbc71
Javier2C <juanortiz20500@gmail.com>
2026-04-23 21:08:42 +0000

    we use the scanf for the first time

--- hello.c
+++ hello.c
@@ -2,5 +2,14 @@
 
 int main(void)
 {
-    printf("Hello Javier! \n");
+    char name[50];
+    printf("Enter your name: ");
+    //i put the scanf and the %s to make the screen ask you for your name and put a lot of characters
+scanf("%s", &name);
+    printf("Hello %s! \n",name);
+
+    char color[50];
+    printf("enter your favorite color: ");
+    scanf("%s", &color);
+    printf("%s sucks! \n",color );
 }
